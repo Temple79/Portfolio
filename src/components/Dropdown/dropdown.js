68437e4552cc5fa026f1d13afb190b4a94fac470
Temple79 <126321231+Temple79@users.noreply.github.com>
2023-12-24 14:53:03 +0200

    Last changes added

--- src/components/Dropdown/dropdown.js
+++ src/components/Dropdown/dropdown.js
@@ -33,7 +33,7 @@ export default function Dropdown({ data }) {
               {data.issues}
             </p>
             <ul className="techs">
-              <span className="color-change">Langues utilisées</span> :{" "}
+              <span className="color-change-lang">Langues utilisées</span> :{" "}
               {data.lang.map((lang, index) => (
                 <li className="lang" key={index}>
                   {lang}
